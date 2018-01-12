11d9257076fa5aafd160a95adc2d36b819c0cf4e
Eddie <eddiepie@gmail.com>
2018-01-13 10:03:24 +1300

    23_texture_atlas added texture atlas

---
+++ src/23_texture_atlas/light.hh
@@ -0,0 +1,32 @@
+//
+// Created by Eddie Hoyle on 5/11/17.
+//
+
+#ifndef OPENGL_EXAMPLES_17_LIGHT_HH
+#define OPENGL_EXAMPLES_17_LIGHT_HH
+
+
+#include <glm/vec3.hpp>
+
+class Light {
+
+public:
+
+    Light();
+    explicit Light( const glm::vec3& position, const glm::vec3& colour );
+
+    glm::vec3 getPosition() const;
+
+    void setPosition( const glm::vec3& position );
+
+    glm::vec3 getColour() const;
+
+    void setColour( const glm::vec3& colour );
+
+private:
+    glm::vec3 m_position;
+    glm::vec3 m_colour;
+
+};
+
+#endif //OPENGL_EXAMPLES_17_LIGHT_HH
