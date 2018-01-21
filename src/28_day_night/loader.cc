80aa6a4f0309ac9cea04479228c07f662e7e995c
Eddie <eddiepie@gmail.com>
2018-01-21 21:36:08 +1300

    28_day_night fixed texture seams

--- src/28_day_night/loader.cc
+++ src/28_day_night/loader.cc
@@ -82,7 +82,9 @@ GLuint Loader::loadCubeMap( std::vector< std::string > textureFiles ) {
 
     glTexParameteri( GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
     glTexParameteri( GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
-
+    glTexParameteri( GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri( GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    
     m_textures.push_back( texID );
 
     return texID;
