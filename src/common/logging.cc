c043a076c265bda645d8ab4d484da2940f84f435
Eddie <eddiepie@gmail.com>
2018-02-15 21:56:57 +1300

    08_water_soft_edges: Aww yissss soft water edges and  murky water.

---
+++ src/common/logging.cc
@@ -0,0 +1,5 @@
+//
+// Created by Eddie Hoyle on 13/02/18.
+//
+
+#include "logging.hh"
---
+++ src/common/logging.hh
@@ -0,0 +1,9 @@
+#ifndef OPENGL_EXAMPLES_LOGGING_HH
+#define OPENGL_EXAMPLES_LOGGING_HH
+
+#include <mutex>
+
+namespace common {
+}
+
+#endif
